33d9226f24497df6a82c1e38a407594bed7a7978
Jan Hutar <jhutar@redhat.com>
2022-05-13 15:56:48 +0200

    feature: Add API test for /api/users/1 and /api/moves/1 endpoints

--- testing.py
+++ testing.py
@@ -10,5 +10,13 @@ class MyAppUser(HttpUser):
         self.client.get("/api/users")
 
     @task
+    def get_users_uid(self):
+        self.client.get("/api/users/1")
+
+    @task
     def get_moves(self):
         self.client.get("/api/moves")
+
+    @task
+    def get_moves_mid(self):
+        self.client.get("/api/moves/1")
